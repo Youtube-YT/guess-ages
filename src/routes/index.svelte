8b7a188604bd26f118c4c6324baf1af512fbe22f
Youtube-YT <mingweiop@gmail.com>
2021-12-19 03:28:46 +0000

    commit 1

---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+    let age = "";
+    let name = "";
+    async function getAge() {
+      fetch('https://api.agify.io/?name=' + age)
+        .then((response) => response.json())
+        .then((data) => {
+          //Able to change whatever name you like Example: weatherTemparature = data.temperature the name of data should not be changed.
+           age = data.age; // Path for temperature
+          name = data.name; // Path for description
+        });
+    }
+        </script>
+        <p>Ages: {age}</p>
